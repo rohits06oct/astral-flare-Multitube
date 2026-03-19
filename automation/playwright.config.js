6a2b1301cdaa1a434d4b87ed2b1a680989f99ec1
Rohit Singhal <rohits.06oct@gmail.com>
2026-03-19 19:55:11 +0530

    Blogs Opt

--- automation/playwright.config.js
+++ automation/playwright.config.js
@@ -2,7 +2,7 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
     testDir: './tests',
-    fullyParallel: true,
+    fullyParallel: false,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
@@ -33,10 +33,10 @@ module.exports = defineConfig({
             name: 'mobile-pixel',
             use: { ...devices['Pixel 5'] },
         },
-        {
-            name: 'desktop-firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //     name: 'desktop-firefox',
+        //     use: { ...devices['Desktop Firefox'] },
+        // },
         {
             name: 'mobile-iphone',
             use: { ...devices['iPhone 15'] },
